f1b4c6f62ecfa0d68e83e02af6b43f239cc11435
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-09-02 19:57:23 +0530

    feat: routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import postRoutes from './routes/posts.js';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/post', postRoutes);
+
+app.use('/api/auth', authRoutes);
+
+app.use('/api/user', userRoutes);
+
+app.get('/', (req, res) => {
+	res.json('Hello this is the backend');
+});
 
 app.listen(8800, () => {
 	console.log('Connected to Backend');
