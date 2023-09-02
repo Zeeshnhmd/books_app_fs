5a5aab6eb385fbdfe91755c02cfe95e49038f169
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-09-02 23:40:50 +0530

    feat: cookie parser cors dotenv

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/post', postRoutes);
 
