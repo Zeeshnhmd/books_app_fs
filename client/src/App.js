d9319d6a7aa0dc85d58d849b07f4fee3fb70106a
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-09-02 23:42:14 +0530

    feat: client changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
+import { Button, Form, Input } from 'antd';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const onFinish = (values) => {
+		axios.post('http://localhost:8800/api/auth/register', values);
+		console.log('Success:', values);
+	};
+	const onFinishFailed = (errorInfo) => {
+		console.log('Failed:', errorInfo);
+	};
+
+	const onFinishlogin = (values) => {
+		axios.post('http://localhost:8800/api/auth/login', values);
+		console.log('Success:', values);
+	};
+
+	return (
+		<>
+			<Form
+				name="basic"
+				labelCol={{
+					span: 8,
+				}}
+				wrapperCol={{
+					span: 16,
+				}}
+				style={{
+					maxWidth: 600,
+				}}
+				initialValues={{
+					remember: true,
+				}}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete="off"
+			>
+				<Form.Item
+					label="Username"
+					name="username"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your username!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					label="Email"
+					name="email"
+					rules={[
+						{
+							required: true,
+
+							message: 'Please input your username!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+
+				<Form.Item
+					label="Password"
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your password!',
+						},
+					]}
+				>
+					<Input.Password />
+				</Form.Item>
+
+				<Form.Item
+					wrapperCol={{
+						offset: 8,
+						span: 16,
+					}}
+				>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+
+			<Form
+				name="basic"
+				labelCol={{
+					span: 8,
+				}}
+				wrapperCol={{
+					span: 16,
+				}}
+				style={{
+					maxWidth: 600,
+				}}
+				initialValues={{
+					remember: true,
+				}}
+				onFinish={onFinishlogin}
+				onFinishFailed={onFinishFailed}
+				autoComplete="off"
+			>
+				<Form.Item
+					label="Username"
+					name="username"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your username!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+
+				<Form.Item
+					label="Password"
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your password!',
+						},
+					]}
+				>
+					<Input.Password />
+				</Form.Item>
+
+				<Form.Item
+					wrapperCol={{
+						offset: 8,
+						span: 16,
+					}}
+				>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
+	);
 }
 
 export default App;
