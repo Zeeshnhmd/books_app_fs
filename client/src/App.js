95db04b2b8b9241f6e5011252c978c55cc7b14b2
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-09-04 20:17:56 +0530

    feat: route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,149 +1,15 @@
-import { Button, Form, Input } from 'antd';
-import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
 
 import './App.css';
 
 function App() {
-	const onFinish = (values) => {
-		axios.post('http://localhost:8800/api/auth/register', values);
-		console.log('Success:', values);
-	};
-	const onFinishFailed = (errorInfo) => {
-		console.log('Failed:', errorInfo);
-	};
-
-	const onFinishlogin = (values) => {
-		axios.post('http://localhost:8800/api/auth/login', values);
-		console.log('Success:', values);
-	};
-
 	return (
-		<>
-			<Form
-				name="basic"
-				labelCol={{
-					span: 8,
-				}}
-				wrapperCol={{
-					span: 16,
-				}}
-				style={{
-					maxWidth: 600,
-				}}
-				initialValues={{
-					remember: true,
-				}}
-				onFinish={onFinish}
-				onFinishFailed={onFinishFailed}
-				autoComplete="off"
-			>
-				<Form.Item
-					label="Username"
-					name="username"
-					rules={[
-						{
-							required: true,
-							message: 'Please input your username!',
-						},
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item
-					label="Email"
-					name="email"
-					rules={[
-						{
-							required: true,
-
-							message: 'Please input your username!',
-						},
-					]}
-				>
-					<Input />
-				</Form.Item>
-
-				<Form.Item
-					label="Password"
-					name="password"
-					rules={[
-						{
-							required: true,
-							message: 'Please input your password!',
-						},
-					]}
-				>
-					<Input.Password />
-				</Form.Item>
-
-				<Form.Item
-					wrapperCol={{
-						offset: 8,
-						span: 16,
-					}}
-				>
-					<Button type="primary" htmlType="submit">
-						Submit
-					</Button>
-				</Form.Item>
-			</Form>
-
-			<Form
-				name="basic"
-				labelCol={{
-					span: 8,
-				}}
-				wrapperCol={{
-					span: 16,
-				}}
-				style={{
-					maxWidth: 600,
-				}}
-				initialValues={{
-					remember: true,
-				}}
-				onFinish={onFinishlogin}
-				onFinishFailed={onFinishFailed}
-				autoComplete="off"
-			>
-				<Form.Item
-					label="Username"
-					name="username"
-					rules={[
-						{
-							required: true,
-							message: 'Please input your username!',
-						},
-					]}
-				>
-					<Input />
-				</Form.Item>
-
-				<Form.Item
-					label="Password"
-					name="password"
-					rules={[
-						{
-							required: true,
-							message: 'Please input your password!',
-						},
-					]}
-				>
-					<Input.Password />
-				</Form.Item>
-
-				<Form.Item
-					wrapperCol={{
-						offset: 8,
-						span: 16,
-					}}
-				>
-					<Button type="primary" htmlType="submit">
-						Submit
-					</Button>
-				</Form.Item>
-			</Form>
-		</>
+		<Routes>
+			<Route element={<SignIn />} path="/signin" />
+			<Route element={<SignUp />} path="/signup" />
+		</Routes>
 	);
 }
 
